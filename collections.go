afb3c3a422fb99a512f0723aa83d44078a8bba6a
Jacob O'Toole <jacob@jotpot.co.uk>
2017-11-13 15:12:18 +0000

    Use relative paths for imports

--- collections.go
+++ collections.go
@@ -2,18 +2,18 @@
 //Made in a couple of computer science lessons, and a bit for fun too.
 package collections
 
-import "collections/list"
-import "collections/queue"
-import "collections/priorityQueue"
-import "collections/stack"
+import "./list"
+import "./queue"
+import "./priorityQueue"
+import "./stack"
 
-//List is list.List from collections/list
+//List is list.List from ./list
 type List list.List
-//ListItem is list.Item from collections/list
+//ListItem is list.Item from ./list
 type ListItem list.Item
-//Queue is queue.Queue from collections/queue
+//Queue is queue.Queue from ./queue
 type Queue queue.Queue
-//PriorityQueue is priorityQueue.PriorityQueue from collections/priorityQueue
+//PriorityQueue is priorityQueue.PriorityQueue from ./priorityQueue
 type PriorityQueue priorityQueue.PriorityQueue
-//Stack is stack.Stack from collections/stack
+//Stack is stack.Stack from ./stack
 type Stack stack.Stack
